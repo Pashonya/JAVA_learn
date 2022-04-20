93cd46468240353687a8315145ca55934aa1ddf2
Pashonya <rodionov.pavel.7@gmail.com>
2022-04-20 14:08:54 +0300

    Task_01 done

---
+++ 02_module/src/task_02/Main.java
@@ -0,0 +1,48 @@
+package task_02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int houstonAge = 3;
+        int geraltAge = 2;
+        int basiaAge = 4;
+
+        int min = -1;
+        int middle = -1;
+        int max = -1;
+
+        //min
+
+        if (houstonAge <= geraltAge && houstonAge <= basiaAge) {
+            min = houstonAge;
+        } else if (geraltAge <= houstonAge && geraltAge <= basiaAge) {
+            min = geraltAge;
+        } else {
+            System.out.println("min = basiaAge");
+        }
+
+        //middle
+
+        if ((houstonAge <= geraltAge && houstonAge >= basiaAge) || (houstonAge >= geraltAge && houstonAge <= basiaAge)) {
+            middle = houstonAge;
+        } else if ((geraltAge <= houstonAge && geraltAge >= basiaAge) || (geraltAge >= houstonAge && geraltAge <= basiaAge)) {
+            middle = geraltAge;
+        } else {
+            System.out.println("middle = basiaAge");
+        }
+
+        //max
+
+        if (houstonAge >= geraltAge && houstonAge >= basiaAge)
+            max = houstonAge;
+        else if (geraltAge >= houstonAge && geraltAge >= basiaAge) {
+            max = geraltAge;
+        } else {
+            System.out.println("max = basiaAge");
+        }
+
+        System.out.println("Minimal Age: " + min);
+        System.out.println("Middle Age: " + middle);
+        System.out.println("Maximum Age: " + max);
+    }
+}
