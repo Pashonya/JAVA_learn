fc33ed648cbedf64f4e97eeb777efbff17bf5cc0
Pashonya <rodionov.pavel.7@gmail.com>
2022-04-27 01:41:45 +0300

    Task_02 done

---
+++ 03_module/src/task_02/Main.java
@@ -0,0 +1,24 @@
+package task_02;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите число: ");
+
+        int value = new Scanner(System.in).nextInt();
+
+        for (int i = 1; i <= value; i = i + 1) {
+
+            for (int j = value; j >= 1; j--) {
+                if (value % j != 0)
+                    continue;
+
+                if (value == i * j)
+                    System.out.println(i + "*" + j);
+            }
+        }
+    }
+}
+
+
