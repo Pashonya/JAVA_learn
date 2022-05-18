83354357629d91a1884243d1b11334b017095ada
Pashonya <rodionov.pavel.7@gmail.com>
2022-05-18 13:53:47 +0300

    Modules_04 done

--- 04_module/src/tasck_03/Main.java
+++ 04_module/src/tasck_03/Main.java
@@ -3,8 +3,8 @@ package tasck_03;
 public class Main {
     public static void main(String[] args) {
         Printer documents = new Printer();
-        documents.append("ГПХ","green", 3);
-        documents.append("ИП","red");
+        documents.append("ГПХ","green", 9);
+        documents.append("ИП","red", 6);
         documents.print("");
 
 
--- 04_module/src/tasck_03/Printer.java
+++ 04_module/src/tasck_03/Printer.java
@@ -4,7 +4,6 @@ public class Printer {
 
     private static String queue = "";
     private static int pendingPagesCount = 0;
-    private static int quantity = 0;
     private static int allPagesCount = 0;
 
 
@@ -12,32 +11,19 @@ public class Printer {
         queue = "Документы в печати:";
     }
 
-    public static int getPendingPagesCount() {
-        return pendingPagesCount;
-    }
-    public static int getAllPagesCount () {
-        return allPagesCount;
-    }
-
-
-    public void append(String name) {
-        append(name, "");
-    }
-
-    public void append(String name, String inkСolor) {
-        append(name, inkСolor, 1);
+    public void append(String name, String inkColor) {
+        append(name, inkColor, 1);
     }
 
     public void append(String name, String inkColor, int quantity) {
 
-        boolean error = false;
 
         if (contains(name)) {
             return;
         }
         queue = queue + "\n" + name + " " + inkColor + " "+ quantity + " ";
         pendingPagesCount = pendingPagesCount + quantity;
-        allPagesCount = allPagesCount;
+        allPagesCount = allPagesCount + quantity;
     }
 
     public void clear() {
@@ -59,8 +45,8 @@ public class Printer {
         System.out.println("Общее количество страниц: " + pendingPagesCount + " - стр.");
         clear();
         System.out.println();
-        System.out.println("Количество страниц на печать: " + pendingPagesCount);
-        System.out.println("Всего страниц было в печати: " + allPagesCount);
+        System.out.println("Количество страниц на печать: " + pendingPagesCount + " - стр.");
+        System.out.println("Всего страниц было в печати: " + allPagesCount + " - стр.");
         clear();
     }
 }
